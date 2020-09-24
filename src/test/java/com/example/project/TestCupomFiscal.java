11192a1269eed0dd933debd8b776315846bb2e9c
Rodrigo Vilar <rvilar@gmail.com>
2020-09-23 22:08:51 -0300

    Testes para o Exercício 7

--- src/test/java/com/example/project/TestCupomFiscal.java
+++ src/test/java/com/example/project/TestCupomFiscal.java
@@ -8,203 +8,168 @@ import org.junit.jupiter.api.Test;
 public class TestCupomFiscal {
 
 	private String BREAK = System.lineSeparator();
-	
-	private String TEXTO_ESPERADO_LOJA_COMPLETA = "Loja 1" + BREAK +
-			"Log 1, 10 C1" + BREAK +
-			"Bai 1 - Mun 1 - E1" + BREAK +
-			"CEP:11111-111 Tel (11) 1111-1111" + BREAK +
-			"Obs 1" + BREAK +
-			"CNPJ: 11.111.111/1111-11" + BREAK +
-			"IE: 123456789" + BREAK;
 
-	private String TEXTO_ESPERADO_SEM_NUMERO = "Loja 1" + BREAK +
-			"Log 1, s/n C1" + BREAK +
-			"Bai 1 - Mun 1 - E1" + BREAK +
-			"CEP:11111-111 Tel (11) 1111-1111" + BREAK +
-			"Obs 1" + BREAK +
-			"CNPJ: 11.111.111/1111-11" + BREAK +
-			"IE: 123456789" + BREAK;
+	private String TEXTO_ESPERADO_LOJA_COMPLETA = "Loja 1" + BREAK + "Log 1, 10 C1" + BREAK + "Bai 1 - Mun 1 - E1"
+			+ BREAK + "CEP:11111-111 Tel (11) 1111-1111" + BREAK + "Obs 1" + BREAK + "CNPJ: 11.111.111/1111-11" + BREAK
+			+ "IE: 123456789" + BREAK;
 
-	private String TEXTO_ESPERADO_SEM_COMPLEMENTO = "Loja 1" + BREAK +
-			"Log 1, 10" + BREAK +
-			"Bai 1 - Mun 1 - E1" + BREAK +
-			"CEP:11111-111 Tel (11) 1111-1111" + BREAK +
-			"Obs 1" + BREAK +
-			"CNPJ: 11.111.111/1111-11" + BREAK +
-			"IE: 123456789" + BREAK;
+	private String TEXTO_ESPERADO_SEM_NUMERO = "Loja 1" + BREAK + "Log 1, s/n C1" + BREAK + "Bai 1 - Mun 1 - E1" + BREAK
+			+ "CEP:11111-111 Tel (11) 1111-1111" + BREAK + "Obs 1" + BREAK + "CNPJ: 11.111.111/1111-11" + BREAK
+			+ "IE: 123456789" + BREAK;
 
-	private String TEXTO_ESPERADO_SEM_BAIRRO = "Loja 1" + BREAK +
-			"Log 1, 10 C1" + BREAK +
-			"Mun 1 - E1" + BREAK +
-			"CEP:11111-111 Tel (11) 1111-1111" + BREAK +
-			"Obs 1" + BREAK +
-			"CNPJ: 11.111.111/1111-11" + BREAK +
-			"IE: 123456789" + BREAK;
+	private String TEXTO_ESPERADO_SEM_COMPLEMENTO = "Loja 1" + BREAK + "Log 1, 10" + BREAK + "Bai 1 - Mun 1 - E1"
+			+ BREAK + "CEP:11111-111 Tel (11) 1111-1111" + BREAK + "Obs 1" + BREAK + "CNPJ: 11.111.111/1111-11" + BREAK
+			+ "IE: 123456789" + BREAK;
 
-	private String TEXTO_ESPERADO_SEM_CEP = "Loja 1" + BREAK +
-			"Log 1, 10 C1" + BREAK +
-			"Bai 1 - Mun 1 - E1" + BREAK +
-			"Tel (11) 1111-1111" + BREAK +
-			"Obs 1" + BREAK +
-			"CNPJ: 11.111.111/1111-11" + BREAK +
-			"IE: 123456789" + BREAK;
+	private String TEXTO_ESPERADO_SEM_BAIRRO = "Loja 1" + BREAK + "Log 1, 10 C1" + BREAK + "Mun 1 - E1" + BREAK
+			+ "CEP:11111-111 Tel (11) 1111-1111" + BREAK + "Obs 1" + BREAK + "CNPJ: 11.111.111/1111-11" + BREAK
+			+ "IE: 123456789" + BREAK;
 
-	private String TEXTO_ESPERADO_SEM_TELEFONE = "Loja 1" + BREAK +
-			"Log 1, 10 C1" + BREAK +
-			"Bai 1 - Mun 1 - E1" + BREAK +
-			"CEP:11111-111" + BREAK +
-			"Obs 1" + BREAK +
-			"CNPJ: 11.111.111/1111-11" + BREAK +
-			"IE: 123456789" + BREAK;
+	private String TEXTO_ESPERADO_SEM_CEP = "Loja 1" + BREAK + "Log 1, 10 C1" + BREAK + "Bai 1 - Mun 1 - E1" + BREAK
+			+ "Tel (11) 1111-1111" + BREAK + "Obs 1" + BREAK + "CNPJ: 11.111.111/1111-11" + BREAK + "IE: 123456789"
+			+ BREAK;
 
-	private String TEXTO_ESPERADO_SEM_OBSERVACAO = "Loja 1" + BREAK +
-			"Log 1, 10 C1" + BREAK +
-			"Bai 1 - Mun 1 - E1" + BREAK +
-			"CEP:11111-111 Tel (11) 1111-1111" + BREAK +
-			"" + BREAK +
-			"CNPJ: 11.111.111/1111-11" + BREAK +
-			"IE: 123456789" + BREAK;
+	private String TEXTO_ESPERADO_SEM_TELEFONE = "Loja 1" + BREAK + "Log 1, 10 C1" + BREAK + "Bai 1 - Mun 1 - E1"
+			+ BREAK + "CEP:11111-111" + BREAK + "Obs 1" + BREAK + "CNPJ: 11.111.111/1111-11" + BREAK + "IE: 123456789"
+			+ BREAK;
 
-	@BeforeAll
-	public void setup() {
-		CupomFiscal.NOME_LOJA = "Loja 1";
-		CupomFiscal.LOGRADOURO = "Log 1";
-		CupomFiscal.NUMERO = 10;
-		CupomFiscal.COMPLEMENTO = "C1";
-		CupomFiscal.BAIRRO = "Bai 1";
-		CupomFiscal.MUNICIPIO = "Mun 1";
-		CupomFiscal.ESTADO = "E1";
-		CupomFiscal.CEP = "11111-111";
-		CupomFiscal.TELEFONE = "(11) 1111-1111";
-		CupomFiscal.OBSERVACAO = "Obs 1";
-		CupomFiscal.CNPJ = "11.111.111/1111-11";
-		CupomFiscal.INSCRICAO_ESTADUAL = "123456789";
-	}
+	private String TEXTO_ESPERADO_SEM_OBSERVACAO = "Loja 1" + BREAK + "Log 1, 10 C1" + BREAK + "Bai 1 - Mun 1 - E1"
+			+ BREAK + "CEP:11111-111 Tel (11) 1111-1111" + BREAK + "" + BREAK + "CNPJ: 11.111.111/1111-11" + BREAK
+			+ "IE: 123456789" + BREAK;
+
+	private String NOME_LOJA = "Loja 1";
+	private String LOGRADOURO = "Log 1";
+	private int NUMERO = 10;
+	private String COMPLEMENTO = "C1";
+	private String BAIRRO = "Bai 1";
+	private String MUNICIPIO = "Mun 1";
+	private String ESTADO = "E1";
+	private String CEP = "11111-111";
+	private String TELEFONE = "(11) 1111-1111";
+	private String OBSERVACAO = "Obs 1";
+	private String CNPJ = "11.111.111/1111-11";
+	private String INSCRICAO_ESTADUAL = "123456789";
 
 	@Test
 	public void lojaCompleta() {
-		rodarTestarRetorno(TEXTO_ESPERADO_LOJA_COMPLETA);
+		rodarTestarRetorno(TEXTO_ESPERADO_LOJA_COMPLETA, NOME_LOJA, LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO, MUNICIPIO,
+				ESTADO, CEP, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void nomeVazio() {
-		CupomFiscal.NOME_LOJA = "";
-		verificarCampoObrigatorio("O campo nome da loja é obrigatório");
-		CupomFiscal.NOME_LOJA = "Arcos Dourados Com. de Alimentos LTDA";
+		verificarCampoObrigatorio("O campo nome da loja é obrigatório", "", LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO,
+				MUNICIPIO, ESTADO, CEP, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void logradouroVazio() {
-		CupomFiscal.LOGRADOURO = "";
-		verificarCampoObrigatorio("O campo logradouro do endereço é obrigatório");
-		CupomFiscal.LOGRADOURO = "Av. Projetada Leste";
+		verificarCampoObrigatorio("O campo logradouro do endereço é obrigatório", NOME_LOJA, "", NUMERO, COMPLEMENTO,
+				BAIRRO, MUNICIPIO, ESTADO, CEP, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void numeroZero() {
-		CupomFiscal.NUMERO = 0;
-		rodarTestarRetorno(TEXTO_ESPERADO_SEM_NUMERO);
-		CupomFiscal.NUMERO = 500;
+		rodarTestarRetorno(TEXTO_ESPERADO_SEM_NUMERO, NOME_LOJA, LOGRADOURO, 0, COMPLEMENTO, BAIRRO, MUNICIPIO, ESTADO,
+				CEP, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void complementoVazio() {
-		CupomFiscal.COMPLEMENTO = "";
-		rodarTestarRetorno(TEXTO_ESPERADO_SEM_COMPLEMENTO);
-		CupomFiscal.COMPLEMENTO = "C1";
+		rodarTestarRetorno(TEXTO_ESPERADO_SEM_COMPLEMENTO, NOME_LOJA, LOGRADOURO, NUMERO, "", BAIRRO, MUNICIPIO, ESTADO,
+				CEP, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void bairroVazio() {
-		CupomFiscal.BAIRRO = "";
-		rodarTestarRetorno(TEXTO_ESPERADO_SEM_BAIRRO);
-		CupomFiscal.BAIRRO = "Bai 1";
+		rodarTestarRetorno(TEXTO_ESPERADO_SEM_BAIRRO, NOME_LOJA, LOGRADOURO, NUMERO, COMPLEMENTO, "", MUNICIPIO, ESTADO,
+				CEP, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void municipioVazio() {
-		CupomFiscal.MUNICIPIO = "";
-		verificarCampoObrigatorio("O campo município do endereço é obrigatório");
-		CupomFiscal.MUNICIPIO = "Campinas";
+		verificarCampoObrigatorio("O campo município do endereço é obrigatório", NOME_LOJA, LOGRADOURO, NUMERO,
+				COMPLEMENTO, BAIRRO, "", ESTADO, CEP, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void estadoVazio() {
-		CupomFiscal.ESTADO = "";
-		verificarCampoObrigatorio("O campo estado do endereço é obrigatório");
-	    CupomFiscal.ESTADO = "SP";
+		verificarCampoObrigatorio("O campo estado do endereço é obrigatório", NOME_LOJA, LOGRADOURO, NUMERO,
+				COMPLEMENTO, BAIRRO, MUNICIPIO, "", CEP, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void cepVazio() {
-		CupomFiscal.CEP = "";
-		rodarTestarRetorno(TEXTO_ESPERADO_SEM_CEP);
-	    CupomFiscal.CEP = "11111-111";
+		rodarTestarRetorno(TEXTO_ESPERADO_SEM_CEP, NOME_LOJA, LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO, MUNICIPIO,
+				ESTADO, null, TELEFONE, OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void telefoneVazio() {
-		CupomFiscal.TELEFONE = "";
-		rodarTestarRetorno(TEXTO_ESPERADO_SEM_TELEFONE);
-	    CupomFiscal.TELEFONE = "(11) 1111-1111";
+		rodarTestarRetorno(TEXTO_ESPERADO_SEM_TELEFONE, NOME_LOJA, LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO, MUNICIPIO,
+				ESTADO, CEP, "", OBSERVACAO, CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void observacaoVazia() {
-		CupomFiscal.OBSERVACAO = "";
-		rodarTestarRetorno(TEXTO_ESPERADO_SEM_OBSERVACAO);
-	    CupomFiscal.OBSERVACAO = "Obs 1";
+		rodarTestarRetorno(TEXTO_ESPERADO_SEM_OBSERVACAO, NOME_LOJA, LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO, MUNICIPIO,
+				ESTADO, CEP, TELEFONE, "", CNPJ, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void cnpjVazio() {
-		CupomFiscal.CNPJ = "";
-		verificarCampoObrigatorio("O campo CNPJ da loja é obrigatório");
-	    CupomFiscal.CNPJ = "42.591.651/0797-34";
+		verificarCampoObrigatorio("O campo CNPJ da loja é obrigatório", NOME_LOJA, LOGRADOURO, NUMERO, COMPLEMENTO,
+				BAIRRO, MUNICIPIO, ESTADO, CEP, TELEFONE, OBSERVACAO, null, INSCRICAO_ESTADUAL);
 	}
 
 	@Test
 	public void inscricaoEstadualVazia() {
-		CupomFiscal.INSCRICAO_ESTADUAL = "";
-		verificarCampoObrigatorio("O campo inscrição estadual da loja é obrigatório");
-		CupomFiscal.INSCRICAO_ESTADUAL = "244.898.500.113";
+		verificarCampoObrigatorio("O campo inscrição estadual da loja é obrigatório", NOME_LOJA, LOGRADOURO, NUMERO,
+				COMPLEMENTO, BAIRRO, MUNICIPIO, ESTADO, CEP, TELEFONE, OBSERVACAO, CNPJ, "");
 	}
-	
+
 	@Test
 	public void exercicio02_Customizado() {
-		//Defina seus próprios valores para as variáveis a seguir 
-		CupomFiscal.NOME_LOJA = "";
-		CupomFiscal.LOGRADOURO = "";
-		CupomFiscal.NUMERO = 0;
-		CupomFiscal.COMPLEMENTO = "";
-		CupomFiscal.BAIRRO = "";
-		CupomFiscal.MUNICIPIO = "";
-		CupomFiscal.ESTADO = "";
-		CupomFiscal.CEP = "";
-		CupomFiscal.TELEFONE = "";
-		CupomFiscal.OBSERVACAO = "";
-		CupomFiscal.CNPJ = "";
-		CupomFiscal.INSCRICAO_ESTADUAL = "";
-		
-		//E atualize o texto esperado abaixo
-		rodarTestarRetorno("" + BREAK);
+		// Defina seus próprios valores para as variáveis a seguir
+		String nomeLoja = "";
+		String logradouro = "";
+		int numero = 0;
+		String complemento = "";
+		String bairro = "";
+		String municipio = "";
+		String estado = "";
+		String cep = "";
+		String telefone = "";
+		String observacao = "";
+		String cnpj = "";
+		String inscricaoEstadual = "";
+
+		// E atualize o texto esperado abaixo
+		rodarTestarRetorno("" + BREAK, nomeLoja, logradouro, numero, complemento, bairro, municipio, estado, cep,
+				telefone, observacao, cnpj, inscricaoEstadual);
 	}
 
-	private void rodarTestarRetorno(String expected) {
+	private void rodarTestarRetorno(String expected, String nomeLoja, String logradouro, int numero, String complemento,
+			String bairro, String municipio, String estado, String cep, String telefone, String observacao, String cnpj,
+			String inscricaoEstadual) {
 
 		// action
-		String retorno = CupomFiscal.dadosLoja();
+		String retorno = CupomFiscal.dadosLojaParam(nomeLoja, logradouro, numero, complemento, bairro, municipio,
+				estado, cep, telefone, observacao, cnpj, inscricaoEstadual);
 
 		// assertion
 		assertEquals(expected, retorno);
 	}
-	
-	private void verificarCampoObrigatorio(String mensagemEsperada) {
+
+	private void verificarCampoObrigatorio(String mensagemEsperada, String nomeLoja, String logradouro, int numero,
+			String complemento, String bairro, String municipio, String estado, String cep, String telefone,
+			String observacao, String cnpj, String inscricaoEstadual) {
 		try {
-			CupomFiscal.dadosLoja();
+			CupomFiscal.dadosLojaParam(nomeLoja, logradouro, numero, complemento, bairro, municipio, estado, cep,
+					telefone, observacao, cnpj, inscricaoEstadual);
 		} catch (RuntimeException e) {
 			assertEquals(mensagemEsperada, e.getMessage());
 		}
 	}
-	
+
 }
